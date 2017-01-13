937b2cf7ece9280120f2fb1fa8d8a4ec6732f1a5
chase <chase@ChaseLaptop.isd112.net>
2017-01-12 19:24:18 -0600

    removed showblobs

--- Vision Test/src/org/usfirst/frc/team2531/robot/OI.java
+++ Vision Test/src/org/usfirst/frc/team2531/robot/OI.java
@@ -1,9 +1,6 @@
 package org.usfirst.frc.team2531.robot;
 
-import org.usfirst.frc.team2531.robot.commands.ShowBlobs;
-
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -12,9 +9,7 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
 public class OI {
 
 	public static Joystick gamepad = new Joystick(0);
-	public static JoystickButton view = new JoystickButton(gamepad, 1);
 
 	public OI() {
-		view.whileHeld(new ShowBlobs());
 	}
 }
